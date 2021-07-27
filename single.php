e59867affbbb61447bce82e7a68e920da6ec8fe1
Adrien-Ray <a.raymond@codeur.online>
2021-07-27 15:02:33 +0200

    all3

--- single.php
+++ single.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
 <h1>single.php</h1>
+<?php get_header(); ?>
+
+	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    	<h1><?php the_title(); ?></h1>
+    
+    	<?php the_content(); ?>
+
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
 
 <?php get_footer(); ?>
